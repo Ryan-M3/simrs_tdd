d9a1a2af843c8d3b3a0876c9b5fd695d51270711
Ryan McNamara <gn341ram@gmail.com>
2025-09-16 11:32:59 -0700

    Update game event builder tests

--- sim/tests/game_event_sys_test.rs
+++ sim/tests/game_event_sys_test.rs
@@ -1,6 +1,13 @@
 use sim::game_event::GameEventSys;
 
+struct DummyTrigger;
+
 #[test]
 fn can_construct_game_event_sys() {
     let _ = GameEventSys::new();
+}
+
+#[test]
+fn builder_syntax_accepts_trigger() {
+    let _ = GameEventSys::new().with_trigger(DummyTrigger);
 }
